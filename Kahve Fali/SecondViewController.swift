2e162ac6aa7ac24c47f9c44d786fe633d5618035
ycankasal <ycankasal@gmail.com>
2018-11-27 18:36:04 -0500

    Initial Commit

---
+++ Kahve Fali/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kahve Fali
+//
+//  Created by ycankasal on 27.11.2018.
+//  Copyright © 2018 ycankasal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Kahve FaliTests/Kahve_FaliTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kahve_FaliTests.swift
+//  Kahve FaliTests
+//
+//  Created by ycankasal on 27.11.2018.
+//  Copyright © 2018 ycankasal. All rights reserved.
+//
+
+import XCTest
+@testable import Kahve_Fali
+
+class Kahve_FaliTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
